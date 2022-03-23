753936c0dcb141b371eb205adb5cfa57b481d2ad
Marcin Malcher <malcher@student.agh.edu.pl>
2022-03-23 20:53:01 +0100

    Added logging library
    I stole it from our previous course.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,7 +31,9 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "term_io.h"
+#include "ansi.h"
+#include "dbgu.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
