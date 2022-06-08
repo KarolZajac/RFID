e8438401af9d670b51932c7ed5d33d65bde2dc15
KarolZajac <karol.zajac30@gmail.com>
2022-06-08 19:55:15 +0200

    Card loop task handling card data reading/writing.

--- Core/RFID/Src/rfid_task.c
+++ Core/RFID/Src/rfid_task.c
@@ -12,60 +12,53 @@
 #include "../RFID/Inc/rc522_com.h"
 #include "../RFID/Inc/card_com.h"
 
-uint8_t doWrite=1;
+#define BUFFER_SIZE 1024
 
-void dummyLoop()
-{
+uint8_t doWrite = 1;
+static uint8_t buffer[BUFFER_SIZE];
+
+void dummyLoop() {
 	uint8_t rfid_id[16];
 	uint8_t cardKeyA[6];
 
-	uint16_t bufferSize = 16;
-	uint8_t buffer[bufferSize];
+	uint16_t dummyBufferSize = 16;
+	uint8_t dummyBuffer[dummyBufferSize];
 	uint8_t data1[16];
 	uint8_t data2[16];
 
-	for (uint8_t i = 0; i < 6; ++i)
-	{
+	for (uint8_t i = 0; i < 6; ++i) {
 		cardKeyA[i] = 0x0FF;
 	}
-	for (uint8_t i = 0; i < 16; ++i)
-	{
+	for (uint8_t i = 0; i < 16; ++i) {
 		data1[i] = 255;
 		data2[i] = 7;
 	}
-	while (1)
-	{
-		if (rc522_checkCard(rfid_id))
-		{
+	while (1) {
+		if (rc522_checkCard(rfid_id)) {
 
 			xprintf("\nRFID code is: \r\n 0x%02x 0x%02x 0x%02x 0x%02x\n",
 					rfid_id[0], rfid_id[1], rfid_id[2], rfid_id[3]);
 
-			if (card_select(rfid_id) > 0)
-			{
+			if (card_select(rfid_id) > 0) {
 				uint8_t status = card_authenticate(rfid_id, cardKeyA, 4);
 				xprintf("\nAuth status: %d\n", status);
 
-				if (status == 1)
-				{
-					status = card_read(4, buffer);
+				if (status == 1) {
+					status = card_read(4, dummyBuffer);
 
 					xprintf("\nRead status: %d\n", status);
-					xprintf("Read %d bytes\n", bufferSize);
+					xprintf("Read %d bytes\n", dummyBufferSize);
 					xprintf("Read data:\n");
 					for (uint8_t i = 0; i < 16; i += 8)
-						xprintf("%d %d %d %d %d %d %d %d\n", buffer[i],
-								buffer[i + 1], buffer[i + 2], buffer[i + 3],
-								buffer[i + 4], buffer[i + 5], buffer[i + 6],
-								buffer[i + 7]);
+						xprintf("%d %d %d %d %d %d %d %d\n", dummyBuffer[i],
+								dummyBuffer[i + 1], dummyBuffer[i + 2], dummyBuffer[i + 3],
+								dummyBuffer[i + 4], dummyBuffer[i + 5], dummyBuffer[i + 6],
+								dummyBuffer[i + 7]);
 
 					xprintf("Writting data to card\n");
-					if (buffer[0] == 255)
-					{
+					if (dummyBuffer[0] == 255) {
 						status = card_write(4, data2);
-					}
-					else
-					{
+					} else {
 						status = card_write(4, data1);
 					}
 					xprintf("\nWrite status: %d\n", status);
@@ -78,13 +71,63 @@ void dummyLoop()
 	}
 }
 
-void startRfidTask(const void *_)
-{
+void cardTaskLoop() {
+	uint8_t rfid_id[16];
+	uint8_t cardKeyA[6];
+
+	for (uint8_t i = 0; i < 6; ++i) {
+		cardKeyA[i] = 0x0FF;
+	}
+	for (uint8_t i = 0; i < 1024; ++i) {
+		buffer[i] = i / 16;
+	}
+	while (1) {
+		if (rc522_checkCard(rfid_id)) {
+			xprintf("\nRFID code is: \r\n 0x%02x 0x%02x 0x%02x 0x%02x\n",
+					rfid_id[0], rfid_id[1], rfid_id[2], rfid_id[3]);
+
+			if (card_select(rfid_id) > 0) {
+				uint8_t status = card_authenticate(rfid_id, cardKeyA, 4);
+				xprintf("\nAuth status: %d\n", status);
+
+				if (status == 1) {
+					if (doWrite == 0) {
+						xprintf("Writting data to card\n");
+						for (uint8_t i = 0; i < 47; i++) {
+							status = card_write(map_logical_to_physical_addres(i),&buffer[16 * i]);
+							xprintf("\nWrite status to block %d: %d\n", i,status);
+						}
+					} else {
+						for (uint8_t i = 0; i < 47; i++) {
+							status = card_read(map_logical_to_physical_addres(i),&buffer[16 * i]);
+							xprintf("\nBlock %d read status: %d\n", i, status);
+						}
+						xprintf("Read %d bytes\n", BUFFER_SIZE);
+						xprintf("Read data:\n");
+						for (uint8_t i = 0; i < BUFFER_SIZE; i += 8){
+							xprintf("Block %d", i);
+							xprintf("%d %d %d %d %d %d %d %d\n", buffer[i],
+									buffer[i + 1], buffer[i + 2], buffer[i + 3],
+									buffer[i + 4], buffer[i + 5], buffer[i + 6],
+									buffer[i + 7]);
+						}
+
+					}
+					card_stopCrypto();
+				}
+			}
+		}
+		vTaskDelay(100);
+	}
+}
+
+void startRfidTask(const void *_) {
+
 	UNUSED(_);
-	while (1)
-	{
+	while (1) {
 		xprintf("Looping RFID!\n");
-		dummyLoop();
+		//dummyLoop();
+		cardTaskLoop();
 		vTaskDelay(500);
 	}
 
